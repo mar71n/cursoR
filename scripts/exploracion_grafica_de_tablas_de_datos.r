2f01470049894c972db7e6753972e3d9a582854d
mar71n <mar71n@gmail.com>
2013-03-17 08:48:23 -0300

    Asta 8.4

--- scripts/exploracion_grafica_de_tablas_de_datos.r
+++ scripts/exploracion_grafica_de_tablas_de_datos.r
@@ -56,15 +56,15 @@ plot(height.m ~ dbh.cm, data=ufc, xlab = "Diameter (cm)", ylab = "Height (m)")
 # . main="Plot title goes in here"
 # . col="red" makes all the points red. This is specially attractive for overlaid plots.
 
-# ggplot_graficos_calidad.pdf pg.7 
-# Al emplear este paquete es posible utilizar dos funciones: qplot y ggplot. La
-# principal diferencia entre estas dos funciones es que con la segunda se pueden
-# presentar diferentes datos y gracos en un mismo plano cartesiano; mientra que
-# con la primera solo se puede generar un graco por plano cartesiano. En la Fi-
+    # ggplot_graficos_calidad.pdf pg.7 
+    # Al emplear este paquete es posible utilizar dos funciones: qplot y ggplot. La
+    # principal diferencia entre estas dos funciones es que con la segunda se pueden
+    # presentar diferentes datos y gracos en un mismo plano cartesiano; mientra que
+    # con la primera solo se puede generar un graco por plano cartesiano. En la Fi-
 
 library(ggplot2)
 
-qplot(height.m , dbh.cm, data=ufc, xlab = "Diameter (cm)", ylab = "Height (m)")
+qplot( dbh.cm,height.m , data=ufc, xlab = "Diameter (cm)", ylab = "Height (m)")
 
 # 8.1 Organization Parameters
 # From here we have great 
@@ -99,6 +99,91 @@ par(opar)
 # . par(las=1) rotates the y-axis labels to be horizontal rather than vertical.
 # . par(pty="s") forces the plot shape to be square. The alternative is that the plot shape is mutable,
 # which is the default, and is pty="m".
+# . par(new=TRUE) when inserted between plots will plot the next one on the same place as the previous,
+# efecting an overlay. It will not match the axes unless forced to do so. Increasing the mar parameters
+# for the second plot will force it to be printed inside the rst plot.
+# . Various character expansion factors may also be set.
+# You can also interrogate your last graph for details. For example,
+par("usr")
+# [1] 5.920 116.080 1.616 49.784
+# gives you the realized axis limits (compare with Figure 8.1).
+
+# 8.2 Graphical Augmentation
+# A traditional plot, once created, can be augmented using any of a number of dierent tools.
+# The infrastructure of the plot can be altered. For example, axes may be omitted in the initial plot
+# call (axes = FALSE) and added afterwards using the axis() function, which provides greater control and
+# 
+# exibility over the locations and format of the tickmarks, axis labels, and also the content. Note that
+# axis() only provides axis information within the portion of the range of the data, to reduce unnecessary
+# plot clutter. The usual plot frame can be added using the box() function. Text can be located in the
+# margins of the plot, to label certain areas or to augment axis labels, using the mtext() function. A
+# legend can be added using the legend() function, which includes a very useful legend location routine,
+# as shown below. Additions can also be made to the content of the plot, using the points(), lines(),
+# and abline() functions. A number of these dierent steps are shown in gure 8.2.
+
+# 1. Start by creating the plot object, which sets up the dimensions of the space, but omit any plot
+# objects for the moment.
+par(las = 1, mar=c(4,4,3,2))
+plot(ufc$dbh.cm, ufc$height.m, axes=FALSE, xlab="", ylab="", type="n")
+
+# 2. Next, we add the points. Let's use dierent colours for dierent trees.
+points(ufc$dbh.cm, ufc$height.m, col="darkseagreen4")
+points(ufc$dbh.cm[ufc$height.m < 5.0],
+ ufc$height.m[ufc$height.m < 5.0], col="red")
+
+# 3. Add axes. These are the simplest possible calls, we have much greater 
+# exibility than shown here.
+axis(1)
+axis(2)
+
+# 4. Add axis labels using margin text (switching back to vertical for the y-axis).
+par(las=0)
+mtext("Diameter (cm)", side=1, line=3, col="blue")
+mtext("Height (m)", side=2, line=3, col="blue")
+
+# 5. Wrap the plot in the traditional frame.
+box()
+
+# 6. Finally, add a legend.
+legend("bottomright",
+ c("A normal tree", "A weird tree"),
+ col=c("darkseagreen3","red"),
+ pch=c(1,1),
+ bty="n")
+
+# 8.3 Permanence
+# Producing more permanent graphics is just as simple. For example, to create the graphic as a pdf le,
+# which can be imported into various documents, we do the following:
+pdf(file="../graphics/graphic.pdf")
+plot(ufc$dbh.cm, ufc$height.m)
+abline(lm(height.m ~ dbh.cm, data=ufc), col="red")
+dev.off()
+# This will place the pdf in your graphics directory. This is an especially good option if the graphics
+# that you want to produce would ordinarily cover more than one page, for example, if you are producing
+# graphs in a loop. The pdf format is well accepted on the Internet as well, so the graphics are portable.
+# Encapsulated postscript is also supported.
+# Under Windows, you can also copy directly from the plot window to the clipboard as either a metale
+# or a bmp (bitmap) image. Either can then be pasted directly into a Word document, for example.
+# Alternatively using a similar approach to that noted above, one can create a JPEG image which can be
+# imported into Word documents. My experience has been that the vividness of the colour suers using
+# JPEG, and some ameliorative action might be required.
+
+# 8.4 Upgrades
+# The joy of advanced graphics in R is that all the good images can be made using the tools that we've
+# already discussed. And, because the interface is scripted, it's very easy to take graphics that were created
+# for one purpose and evolve them for another. Looping and judicious choice of control parameters can
+# create informative and attractive output. For example, Figure 8.3 is constructed by the following code.
+opar <- par(oma=c(0,0,0,0), mar=c(0,0,0,0))
+x1 <- rep(1:10, 10)
+x2 <- rep(1:10, each=10)
+x3 <- 1:100
+interesting.colour.numbers <- c(1:152,253:259,362:657)
+plot.us <- sample(interesting.colour.numbers, size=max(x3))
+plot(x1, x2, col=colors()[plot.us], pch=20, cex=10, axes=F,
+ ylim=c(0,10), xlim=c(0, 10.5))
+text(x1, x2-0.5, colors()[plot.us], cex=0.3)
+text(x1+0.4, x2-0.4, plot.us, cex=0.5)
+par(opar)
 
 
 
